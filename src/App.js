4835067ce483c4be47186a663024fb953b7e5828
Keisha Gittens <keishamsa@me.com>
2022-01-10 20:13:45 -0600

    simplified style

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import RecipeDetails from './components/RecipeDetails/RecipeDetails';
 function App() {
 	return (
 		<div className='container'>
-			<Nav />
-			<Routes>
-				<Route path='/' element={<Main />} />
-				<Route path='/:id' element={<RecipeDetails />} />
-				<Route path='/about' element={<About />} />
-			</Routes>
+			<nav>
+				<Nav />
+			</nav>
+			<main>
+				<Routes>
+					<Route path='/' element={<Main />} />
+					<Route path='/:id' element={<RecipeDetails />} />
+					<Route path='/about' element={<About />} />
+				</Routes>
+			</main>
 		</div>
 	);
 }

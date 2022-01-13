1c41bc71ec318b44cb76dad7d03f8888d6e0bc72
Keisha Gittens <keishamsa@me.com>
2022-01-12 22:26:34 -0600

    set form state & add event handlers

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Search(props) {
+function Search() {
+    const initialState = {
+			searchInput: '',
+			checkboxInput: '',
+		};
+    const [formState, setFormState] = useState(initialState);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState)
+        setFormState(initialState)
+    };
+
+    const handleChange = (event) => {
+        setFormState({ ...formState, [event.target.id]: event.target.value });
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="searchInput">Search</label>
-            <input type="text" id="searchInput" />
+            <input
+                type="text"
+                id="searchInput"
+                onChange={handleChange}
+                value={formState.searchInput}
+            />
         <label htmlFor='checkboxInput'>Diet preference</label>
-            <select name='checkboxInput' id='checkboxInput'>
+            <select
+                name='checkboxInput'
+                id='checkboxInput'
+                onChange={handleChange}
+                value={formState.checkboxInput}
+            >
                 <option value="">Choose</option>
                 <option value="glutenFree">Gluten Free</option>
                 <option value="keto">Ketogenic</option>
@@ -20,7 +46,7 @@ function Search(props) {
                 <option value="lowFodMap">Low FODMAP</option>
                 <option value="whole30">Whole30</option>
             </select>
-            
+            <button type="submit">Submit</button>
         </form>
         );
     }

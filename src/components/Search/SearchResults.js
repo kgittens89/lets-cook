9f57c81e7de8f57511236d444e7e5e583bcc045f
Keisha Gittens <keishamsa@me.com>
2022-01-17 13:28:42 -0600

    search results linked to details page

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function SearchResults({ results }) {
     return (
-			<>
-            {results.map((result) => {
-                return (
-				<div className='result-box'>
+        <>
+        {results.map((result) => {
+            return (
+                <Link to={`/${result.id}`}>
+                    <div className='result-box'>
                         <h4>{result.title}</h4>
                         <img src={result.image} alt={result.title} />
-				</div>      
-                    )
-                })}
-			</>
-		);
-}
-
-export default SearchResults;
+                    </div>
+                </Link>
+                );
+            })}
+            </>
+            );
+        };
+        
+        export default SearchResults;

2d7e06a19a6ec45fedb520ad1f81da906ed630ac
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-04-21 23:35:46 -0500

    Add MaterialUI search form for possible use

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Search.css'
+import { Card, CardContent, CardActionArea, CardMedia, Typography } from '@mui/material';
 
 
 function SearchResults({ results }) {
@@ -8,13 +9,35 @@ function SearchResults({ results }) {
         <div className='search-results'>
         {results.map((result) => {
             return (
-                <Link to={`/${result.id}`}>
-                    <div className='result-box'>
-                        <img src={result.image} alt={result.title} className='search-image' />
-                        <h4 className='search-title'>{result.title}</h4>
-                    </div>
-                </Link>
-                );
+							<Link to={`/${result.id}`}>
+								<div className='result-box'>
+									<img
+										src={result.image}
+										alt={result.title}
+										className='search-image'
+									/>
+									<h4 className='search-title'>{result.title}</h4>
+								</div>
+								{/* <Card sx={{ maxWidth: 300, m: 1, minHeight: 370 }}>
+									<CardActionArea>
+										<CardMedia
+											component='img'
+											height='140'
+											image={result.image}
+											alt='green iguana'
+										/>
+										<CardContent>
+											<Typography gutterBottom variant='h6' component='div'>
+												{result.title}
+											</Typography>
+											<Typography variant='body2' color='text.secondary'>
+												Lizards are a widespread group of squamate reptiles,
+											</Typography>
+										</CardContent>
+									</CardActionArea>
+								</Card> */}
+							</Link>
+						);
             })}
             </div>
             );

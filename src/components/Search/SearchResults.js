05b797d78894171ab6c3097e4b10d87cb29fd583
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-05-22 13:59:37 -0500

    comment out

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Search.css'
-import { Card, CardContent, CardActionArea, CardMedia, Typography } from '@mui/material';
+// import { Card, CardContent, CardActionArea, CardMedia, Typography } from '@mui/material';
 
 
 function SearchResults({ results, imageNotFound }) {
